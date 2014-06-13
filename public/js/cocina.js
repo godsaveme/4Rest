950884ec40468bc0e83b45ab2ba66a3c900fe8e7
icalre <icalvay@plazaverde.pe>
2014-06-12 19:27:36 -0500

    Corrigiendo error en sonidos, ahora solo reproduce cuando realmente se reciben productos.

--- public/js/cocina.js
+++ public/js/cocina.js
@@ -83,7 +83,9 @@ function recibirpedido(datos, mesa, pedido){
                 for (var i in data) {
                     sumarplatospanel(data[i]['productoid'], data[i]['cantidad'], data[i]['nombre']);
                 }
-                document.getElementById('sonido_recibirpedido').play();
+                if(data.length > 0){
+                    document.getElementById('sonido_recibirpedido').play();
+                }
             }
     });
 }
